df085b09b0b11cb0f0b7834619af39eb96b1eac8
sovhqs <souhailouakkas@gmail.com>
2025-11-08 17:51:39 +0100

    snapshots  extraction script

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-print("Hello, VS Code!")
+## SNAPSHOTS EXTRACTION 
 
-import pandas as pd
+# Racine Opendata officielle (fourni par Vélib' Métropole)
+VELIB_BASE = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole"
 
-print("✅ pandas version :", pd.__version__)
-df = pd.DataFrame({"Nom": ["Sofiane", "Lina"], "Age": [24, 30]})
-print(df)
+# Dossiers de sortie
+OUT_DIR = Path("data/raw/velib")
+OUT_DIR_SNAP = OUT_DIR / "snapshots"
+OUT_DIR.mkdir(parents=True, exist_ok=True)
+OUT_DIR_SNAP.mkdir(parents=True, exist_ok=True)
+
+STATION_INFO_CSV = OUT_DIR / "station_information.csv"
+SYSTEM_INFO_JSON = OUT_DIR / "system_information.json"
+
+print("Sortie:", OUT_DIR.resolve())
